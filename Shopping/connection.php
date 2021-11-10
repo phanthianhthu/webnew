ae81b3caf57e6f934a047b3496dc9b881d1b628c
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-11-11 00:32:15 +0700

    Update connection.php

--- Shopping/connection.php
+++ Shopping/connection.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-    $Connect = pg_connect("postgres://yzfoonhojmrkll:bd52f898a24de3e881f9e3f4ffabbff73c1410ebd352b280ed2ac82d8ae02ee7@ec2-107-20-127-127.compute-1.amazonaws.com:5432/dcau7oi0lua5sv");
+    $Connect = pg_connect("postgres://gettcqzxdyllga:ab954ac871a0798bbf7d5b85deed43a92388d26750445afecdbb2d9548844ef0@ec2-3-227-149-67.compute-1.amazonaws.com:5432/dbdqeuqd7ev7qf");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
