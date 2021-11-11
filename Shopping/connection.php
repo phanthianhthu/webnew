ae17623f16d695a933818e5ed1348afa437eb825
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-11-11 12:06:04 +0700

    Update connection.php

--- Shopping/connection.php
+++ Shopping/connection.php
@@ -1,13 +1,13 @@
 <?php
-    /*$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
+    $conn = mysqli_connect('localhost', 'root', '', 'atnshop') or die("Can not connect database".mysqli_connect_error());
 	
-	mysqli_query($Connect,'SET NAMES "utf8"');
+	//mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-    $Conn = pg_connect("postgres://gettcqzxdyllga:ab954ac871a0798bbf7d5b85deed43a92388d26750445afecdbb2d9548844ef0@ec2-3-227-149-67.compute-1.amazonaws.com:5432/dbdqeuqd7ev7qf");
+    //$Conn = pg_connect("postgres://gettcqzxdyllga:ab954ac871a0798bbf7d5b85deed43a92388d26750445afecdbb2d9548844ef0@ec2-3-227-149-67.compute-1.amazonaws.com:5432/dbdqeuqd7ev7qf");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
-    if (!$Conn) {
+    /*if (!$Conn) {
         die("Connection failed");
-    }
+    }*/
 ?>
